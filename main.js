7b01c9499ad48b57e72a643768e4116b94118f3f
Jonathan Louie <johnlouie88@outlook.com>
2017-06-22 15:36:51 -0700

    server skeleton code

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* global __dirname */
 
+const express = require("express");
+const app = express();
 
+app.get("/library", function (req, res) {});
+app.get("/library/:bookId", function (req, res) {});
+
+app.get("/", function (req, res) {
+    res.sendFile(`${__dirname}/client/index.html`);
+});
+
+app.use(express.static("client"));
+
+app.listen(3000, function () {
+    console.log("Started listening on port 3000!");
+});
