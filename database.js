e62fa851bf8065d0720f9d6474dda8097e6224b2
Johnathan Louie <johnlouie88@outlook.com>
2018-07-12 01:23:10 -0700

    Add insertOne function

--- database.js
+++ database.js
@@ -4,6 +4,31 @@ const MONGODB_URL = "mongodb://localhost/flashcards";
 
 var database = {};
 
+database.insertOne = function (collection, data, callback) {
+    function a1(client) {
+
+        function b1(result) {
+            callback(undefined, result);
+        }
+
+        function b2(error) {
+            callback(error);
+        }
+
+        function c() {
+            return client.close();
+        }
+
+        return client.db().collection(collection).insertOne(data).then(b1, b2).then(c);
+    }
+
+    function a2(error) {
+        callback(error);
+    }
+
+    MongoClient.connect(MONGODB_URL).then(a1, a2);
+};
+
 database.insertMany = function (collection, data, callback) {
     function a1(client) {
 
