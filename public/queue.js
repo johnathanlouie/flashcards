ba7020d02221faf1185642f6fd259be8c560b180
Johnathan Louie <johnlouie88@outlook.com>
2018-07-15 23:50:37 -0700

    Fix bugs

--- public/queue.js
+++ public/queue.js
@@ -1,36 +1,28 @@
-var now = null;
+var now = undefined;
 var prev = [];
 var next = [];
 
-function getNow()
+function get()
 {
     return now;
 }
 
 function prev2()
 {
-    if (prev.length === 0)
+    if (now !== undefined)
     {
-        throw new Error("queue prev array empty");
-    }
-    if (now !== null)
-    {
-        next.unshift(now);
+        next.push(now);
     }
     now = prev.pop();
 }
 
 function next2()
 {
-    if (next.length === 0)
-    {
-        throw new Error("queue next array empty");
-    }
-    if (now !== null)
+    if (now !== undefined)
     {
         prev.push(now);
     }
-    now = next.shift();
+    now = next.pop();
 }
 
 function hasNext()
@@ -47,7 +39,7 @@ function addCards(cards)
 {
     if (!Array.isArray(cards))
     {
-        throw new Error("queue add cards not an array");
+        throw new Error("Not an array.");
     }
     next = next.concat(cards);
 }
@@ -57,26 +49,13 @@ function clear()
     next = [];
 }
 
-// diagnostic function
-function print()
-{
-    console.log("================now===================");
-    console.log(now ? now.term[0].form[0] : undefined);
-    console.log("================next==================");
-    for (let i in next)
-    {
-        console.log(i + ": " + (next[i] ? next[i].term[0].form[0] : undefined));
-    }
-}
-
 const queue = {};
-queue.getNow = getNow;
+queue.get = get;
 queue.prev = prev2;
 queue.next = next2;
 queue.hasNext = hasNext;
 queue.hasPrev = hasPrev;
 queue.addCards = addCards;
 queue.clear = clear;
-queue.print = print;
 
 export default queue;
